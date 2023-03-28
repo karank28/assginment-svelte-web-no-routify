0de3587be005b1f4dfb03bb6106440d6834fa001
Karan Mistry <66006790+karank28@users.noreply.github.com>
2023-03-28 19:02:45 +0530

    Created a project

---
+++ myApp/src/App.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+let appname:string = 'myApp';
+
+import Home from "./components/Home.svelte";
+</script>
+
+
+<header>
+	<div class="border-b-4">
+		<a href="" class="flex items-center">
+			<i class="fa-solid fa-code mx-2"></i>
+			<div class="">
+				<span class="self-center text-xl font-semibold whitespace-nowrap">{appname}</span>
+					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+			</div>
+		</a>
+	</div>
+
+	<nav class="border-b-4">
+		<div class="container items-center justify-between mx-auto">
+			<div class="w-full flex-wrap md:block md:w-auto" id="navbar-default">
+				<ul class="flex flex-col p-4 mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium ">
+					<li>
+					<a href="#" class="block py-2 pl-3 pr-4 md:p-0" aria-current="page">Home</a>
+					</li>
+					<li>
+						<a href="#" class="block py-2 pl-3 pr-4 md:p-0">Products</a>
+					</li>
+					<li>
+						<a href="#" class="block py-2 pl-3 pr-4 md:p-0">Contact Us</a>
+					</li>
+					<li>
+						<a href="#" class="block py-2 pl-3 pr-4 md:p-0">About Us</a>
+					</li>
+					<li>
+						<a href="#" class="block py-2 pl-3 pr-4 md:p-0">Admin</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+</header>  
+
+<main>
+	<Home />
+</main>
+
+<footer class="footer border-t-4">
+    <div class="content has-text-centered">
+        <p>
+            All rights reserved to {appname}
+        </p>
+    </div>
+</footer>
+
---
+++ myApp/src/components/Home.svelte
@@ -0,0 +1,15 @@
+<div class="container">
+    <div class="flex justify-between text-center center p-5">
+        <div class="container border-4 w-64 h-48 m-3 p-4 ">
+            <p class="my-8">Product count</p>
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">View All</button>
+        </div>
+        <div class="container border-4 w-64 h-48 m-3 p-4 ">
+            <p class="my-8">Contact us count</p>
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">Contact us now</button>
+        </div>
+        
+    </div>
+    
+    
+</div>

e337d9a66f39c38e98fc5bac4a346c56cce85d54
Karan Mistry <66006790+karank28@users.noreply.github.com>
2023-03-30 18:06:00 +0530

    #303 - Added New Files

---
+++ myApp/src/components/ContactUs.svelte
@@ -0,0 +1,38 @@
+<div class="p-5 flex">
+    <div class="w-1/2">
+        <form action="" class="w-96 border-4">
+            <div class="my-2">
+                <label for="name" class="block mb-2 text-sm font-medium">Name</label>
+                <input type="text" id="name" class="bg-gray-50 border border-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-3" placeholder="Name" required>
+            </div>
+            <div class="my-2">
+                <label for="email" class="block mb-2 text-sm font-medium">Email-id</label>
+                <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-3" placeholder="Email-id" required>
+            </div>
+            <div class="my-2">
+                <label for="number" class="block mb-2 text-sm font-medium">Contact Number</label>
+                <input type="number" id="number" class="bg-gray-50 border border-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-3" placeholder="Contact Number" required>
+            </div>
+            <div class="my-2">
+                <input class="p-3 border-2 rounded-lg" type="submit" value="Submit">
+                <input class="p-3 border-2 rounded-lg" type="reset" value="Reset">
+            </div>
+        </form>
+    </div>
+    
+    <div class="p-5 w-1/2">
+        <div class="border-4 w-48 h-48 p-5">
+        </div>
+
+        <div>
+            <p>Reach out to us</p>
+            <p>Address Line - 1</p>
+            <p>Address Line - 2</p>
+            <p>Landmark, City</p>
+            <p>State - Zip</p>
+
+        </div>
+    </div>
+</div>
+
+  

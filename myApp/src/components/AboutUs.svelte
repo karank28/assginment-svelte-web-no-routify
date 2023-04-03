f1bff526ac276650af95c31f50f6b3c6f75bf03d
Karan Mistry <66006790+karank28@users.noreply.github.com>
2023-04-03 18:36:26 +0530

    #034 Added Admin File and remove unnecessary file and code. Work on UI part and make fully responsive pages.

--- myApp/src/components/AboutUs.svelte
+++ myApp/src/components/AboutUs.svelte
@@ -1,21 +1,30 @@
 <!-- svelte-ignore a11y-img-redundant-alt -->
-<div class="">
-    <img class="w-full" src="./img/about-us-banner.jpg" alt="image description">
+<div class="w-full">
+    
+    <div class="w-full relative text-center cursor-pointer">
+        <a href="/#">
+            <img class="w-full" src="./img/about-us-banner.jpg" alt="image description">
+        </a>
+        <!-- svelte-ignore a11y-structure -->
+        <figcaption class="w-full absolute flex flex-col items-center text-black inset-y-1/4 max-md:inset-y-2">
+            <div class="text-5xl font-bold uppercase py-1 max-sm:text-3xl max-sm:py-0">About Us</div>
+            <div class="text-xl py-1 max-sm:hidden">Lorem ipsum, dolor sit amet consectetur adipisicing elit.</div>
+        </figcaption>
+    </div>
+
+    <div class="p-6">
+        <div class="my-5 p-3">
+            <p class="text-xl tracking-widest max-sm:text-lg">Hello, my name is</p>
+            <p class="text-5xl font-bold uppercase py-1 max-sm:text-3xl">Karan Mistry</p>
+            <p class="text-lg py-1 max-sm:text-base">I am a front-end web developer. I can provide clean code and pixel perfect design. I also make the website more & more interactive with web animations. I can provide clean code and pixel perfect design. I also make the website more & more interactive with web animations. A responsive design makes your website accessible to all users, regardless of their device. Check out my Portfolio.</p>
+
+            <div class="w-full flex text-center py-3">
+                <a href="/#" class="w-48 bg-sky-500 text-white font-bold py-2 px-4 rounded-lg transition duration-400 hover:scale-110 hover:bg-amber-400 hover:text-black hover:shadow-xl max-sm:w-full max-sm:text-sm max-sm:hover:-translate-x-3">See my work</a>
+            </div>
+            
+
+        </div>
+    </div>
+
 </div>
 
-<div class="container flex p-5" id=ab>
-    <div class="flex-auto p-2 md:p-1 ">
-        <p class="text-lg font-medium ml-2 max-lg:text-base">Hello, my name is</p>
-        <p class="text-5xl font-bold h-fit p-2 max-lg:text-2xl">Karan Mistry</p>
-        <p class="text-lg font-medium ml-2 max-lg:text-base">
-            I am a front-end web developer. I can provide clean code and pixel perfect design. I also make the website more & more interactive with web animations. I can provide clean code and pixel perfect design. I also make the website more & more interactive with web animations. A responsive design makes your website accessible to all users, regardless of their device. Check out my Portfolio.
-        </p>
-        
-        <button class="bg-slate-500 text-white font-bold py-2 px-4 rounded-full mt-10">
-            <a href="/#" class="">See My work</a>
-        </button> 
-    </div>
-    <div class="">
-        
-    </div>
-</div>
